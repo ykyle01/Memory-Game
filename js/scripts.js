712132636d36dae58e429a816e742a699c2d4920
Kyle Yang <kyley@andrew.cmu.edu>
2021-01-09 22:52:59 -0500

    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,13 @@ var maxBlocks = 18;
 var blocks = document.querySelectorAll(".block");
 var colors = [];
 var assignments = [];
+
+// Selected_block == -1 when a block hasn't been selected yet
 var selected_block = -1;
 
 init();
 
+// Initializes the colors and on-click functions
 function init() {
 	initializeColors();
 	setUpBlocks();
@@ -15,6 +18,7 @@ function init() {
 	reset();
 }
 
+// Randomizes colors and adds to list
 function initializeColors() {
 	for(var i = 0; i < Math.floor(maxBlocks/2); i++){
 		do {
@@ -27,19 +31,24 @@ function initializeColors() {
 	}
 }
 
+// Sets up on click function for each block
 function setUpBlocks() {
 	for(var i = 0; i < blocks.length; i++) {
 		blocks[i].addEventListener("click",function() {
 			for(var i = 0; i < blocks.length; i++) {
 				if (this == blocks[i]) {
+					// Show the hidden color
 					this.style.backgroundColor = assignments[i];
+
 					if (selected_block == i) {}
 					else if (selected_block == -1) {
 						selected_block = i;
 					}
+					// If correct pair, keep showing both blocks
 					else if (assignments[selected_block] == assignments[i]) {
 						selected_block = -1;
 					}
+					// If incorrect pair, wait, then hide them again
 					else {
 						setTimeout(function(selected_block, i){
 							blocks[selected_block].style.backgroundColor = "skyblue";
@@ -53,12 +62,14 @@ function setUpBlocks() {
 	}
 }
 
+// Sets up mode buttons on click functions
 function setUpMode() {
 	for(var i = 0; i < modeButtons.length; i++) {
 	modeButtons[i].addEventListener("click",function() {
 		modeButtons[0].classList.remove("selected");
 		modeButtons[1].classList.remove("selected");
 		this.classList.add("selected");
+		// Easy has 8 blocks and restyles HTML
 		if (this.textContent=="Easy") {
 			numBlocks = 8;
 			for (var j = 0; j < numBlocks; j++) {
@@ -66,6 +77,7 @@ function setUpMode() {
 				blocks[j].classList.add("easy");
 			}
 		}
+		// Hard has 18 blocks and restyle HTML
 		else {
 			numBlocks = 18;
 			for (var j = 0; j < numBlocks; j++) {
@@ -78,6 +90,7 @@ function setUpMode() {
 	}
 }
 
+// Assigns new random colors and shows the right number of blocks
 function reset() {
 	assignColors();
 	for(var i = 0; i < maxBlocks; i++) {
@@ -91,7 +104,9 @@ function reset() {
 	}
 }
 
+// Assigns pairs of colors
 function assignColors() {
+	// Creates list of available blocks
 	var availableBlocks = [];
 	for (var i = 0; i < numBlocks; i++) {
 		availableBlocks.push(i);
@@ -101,13 +116,11 @@ function assignColors() {
 	for (var i = 0; i < Math.floor(numBlocks/2); i++) {
 		var temp = Math.floor(Math.random()*availableBlocks.length);
 		var randIndex = availableBlocks[temp];
-		//blocks[randIndex].style.backgroundColor = colors[i];
 		assignments[randIndex] = colors[i];
 		availableBlocks.splice(temp,1);
 
 		temp = Math.floor(Math.random()*availableBlocks.length);
 		randIndex = availableBlocks[temp];
-		//blocks[randIndex].style.backgroundColor = colors[i];
 		assignments[randIndex] = colors[i];
 		availableBlocks.splice(temp,1);
 	}
